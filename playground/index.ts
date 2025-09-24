73572acf54fb24ba5e5e8a3c9c6543014edbdd41
axtk <axtkx@inbox.ru>
2025-09-25 00:19:01 +0300

    add playground

---
+++ playground/index.ts
@@ -0,0 +1,34 @@
+import { Route } from "..";
+import "./index.css";
+
+let route = new Route();
+
+route.on("navigationcomplete", () => {
+  renderHeader();
+  renderMainContent();
+});
+
+route.observe(document);
+
+function renderHeader() {
+  document.querySelector("header")!.className =
+    route.href === "/" ? "full" : "compact";
+}
+
+function renderMainContent() {
+  let { ok, params } = route.match(/^\/sections\/(?<id>\d+)\/?/);
+
+  if (ok)
+    document.querySelector('[data-id="section"] h2 span')!.textContent =
+      params.id ?? "";
+
+  document
+    .querySelector('main[data-id="intro"]')!
+    .toggleAttribute("hidden", ok);
+
+  document
+    .querySelector('main[data-id="section"]')!
+    .toggleAttribute("hidden", !ok);
+
+  document.body.removeAttribute("hidden");
+}
