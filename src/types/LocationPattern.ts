df86bbb1219e59d2cfdd0e7113339c8109e799b8
axtk <axtkx@inbox.ru>
2025-12-15 14:19:53 +0300

    remove generic type parameter based on config type

--- src/types/LocationPattern.ts
+++ src/types/LocationPattern.ts
@@ -1,7 +1,7 @@
 import type { Config } from "./Config.ts";
 import type { LocationValue } from "./LocationValue.ts";
 
-export type LocationPattern<C extends Config = Config> =
-  C["strict"] extends true
+export type LocationPattern =
+  Config["strict"] extends true
     ? LocationValue | LocationValue[]
     : LocationValue | RegExp | (LocationValue | RegExp)[];
--- src/types/LocationValue.ts
+++ src/types/LocationValue.ts
@@ -1,6 +1,6 @@
 import type { Config } from "./Config.ts";
 import type { LocationObject } from "./LocationObject.ts";
 
-export type LocationValue<C extends Config = Config> = C["strict"] extends true
+export type LocationValue = Config["strict"] extends true
   ? LocationObject | undefined
   : string | LocationObject | undefined;
