42c0a5ff87039c719f224bb761c129dcd79a5d40
axtk <axtkx@inbox.ru>
2025-12-20 01:46:46 +0300

    read link href via getAttribute()

--- src/utils/getNavigationOptions.ts
+++ src/utils/getNavigationOptions.ts
@@ -1,13 +1,15 @@
 import type { LinkElement } from "../types/LinkElement.ts";
 import type { NavigationOptions } from "../types/NavigationOptions.ts";
 
-export function getNavigationOptions({ href, target, dataset }: LinkElement) {
+export function getNavigationOptions(element: LinkElement) {
+  let { id, spa, history, scroll } = element.dataset;
+
   return {
-    href,
-    target,
-    spa: dataset.spa,
-    history: dataset.history,
-    scroll: dataset.scroll,
-    id: dataset.id,
+    href: element.getAttribute("href"),
+    target: element.getAttribute("target"),
+    spa,
+    history,
+    scroll,
+    id,
   } as NavigationOptions;
 }
